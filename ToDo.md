8010cc41f92316473e2497a3add53fa293e9f0da
Error 404 <school-exploits@protonmail.com>
2023-01-17 07:13:36 -0600

    Update

--- ToDo.md
+++ ToDo.md
@@ -5,8 +5,8 @@
 - [ ] Add configuration support for guild specific settings [King, Error]
 
 # Other
-- [ ] Add proxy support so that the bot can be hosted on a different machine than the webserver [King, Error] (Maybe Idk yet)
+- [ ] Add proxy support so that the bot can be hosted on a different machine than the webserver [King, Error]
 - [ ] Add dashboard configuration support for bots [King, Error]
 
 # Needs Done ASAP
-- [ ] Find out why the bot (example.js) is not detecting messages [King, Error]
+- [ ] Fix nyxx.config.json not found error when using nyxx from node_modules [King, Error]
--- index.js
+++ index.js
@@ -1,115 +1,131 @@
 const { argv, exit } = require("process");
-const prompts = require('prompts');
-const fs = require('fs');
-let config = require(".../nyxx.config.json");
+const prompts = require("prompts");
+const fs = require("fs");
+const { join } = require("path");
+const config = require(join(__dirname, "./nyxx.config.json"));
 
 async function check() {
-    if (argv[2] === "init") {
-        const name = await prompts({
-            type: 'text',
-            name: 'value',
-            message: 'What is the name of your project?'
-        });
+  if (argv[2] === "init") {
+    const name = await prompts({
+      type: "text",
+      name: "value",
+      message: "What is the name of your project?",
+    });
 
-        const dashboard = await prompts({
-            type: 'select',
-            name: 'value',
-            message: 'What dashboard would you like to use?',
-            choices: [
-                { title: 'None (Make your own)', value: 'none' },
-                { title: '(Add Later)', value: 'addlater' },
-            ]
-        });
+    const dashboard = await prompts({
+      type: "select",
+      name: "value",
+      message: "What dashboard would you like to use?",
+      choices: [
+        { title: "None (Make your own)", value: "none" },
+        { title: "(Add Later)", value: "addlater" },
+      ],
+    });
 
-        const dashbase = await prompts({
-            type: 'select',
-            name: 'value',
-            message: 'What database would you like to use for configurations with your bot for guilds?',
-            choices: [
-                { title: 'MongoDB', value: 'mongo' },
-                { title: 'Local JSON', value: 'json' },
-            ]
-        });
-        
-        if (dashboard.value === "preset") {
-            const preset = await prompts({
-                type: 'select',
-                name: 'value',
-                message: 'Which preset?',
-                choices: [
-                    { title: 'Soft', value: 'soft' },
-                    { title: '6PG', value: '6pg' },
-                    { title: 'Custom', value: 'custom' }
-                ]
-            });
-        }
+    const dashbase = await prompts({
+      type: "select",
+      name: "value",
+      message:
+        "What database would you like to use for configurations with your bot for guilds?",
+      choices: [
+        { title: "MongoDB", value: "mongo" },
+        { title: "Local JSON", value: "json" },
+      ],
+    });
+
+    if (dashboard.value === "preset") {
+      const preset = await prompts({
+        type: "select",
+        name: "value",
+        message: "Which preset?",
+        choices: [
+          { title: "Soft", value: "soft" },
+          { title: "6PG", value: "6pg" },
+          { title: "Custom", value: "custom" },
+        ],
+      });
     }
+  }
 }
 
 check();
-    
 
 module.exports = {
-    sync: function(client) {
-        let config = require("./nyxx.config.json");
+  sync: function (client) {
+    if (!client) throw new Error("You must provide a client to sync!");
+    if (!config.database)
+      throw new Error(
+        "You must have a database in your nyxx.config.json file!"
+      );
 
-        if (!client) throw new Error("You must provide a client to sync!");
-        if (!config.database) throw new Error("You must have a database in your nyxx.config.json file!");
+    if (config.database.startsWith("local")) {
+      let split = config.database.split(" ");
+      if (!split[1])
+        throw new Error("You must provide a path to your database!");
+      if (!fs.existsSync(split[1]))
+        throw new Error("The path you provided does not exist!");
 
-        if (config.database.startsWith("local")) {
-            let split = config.database.split(" ");
-            if (!split[1]) throw new Error("You must provide a path to your database!");
-            if (!fs.existsSync(split[1])) throw new Error("The path you provided does not exist!");
-
-            global.db = require("./" + split[1]);
-
-
-        }
-    },
-
-    prefix: function(guild) {
-        if (!config.prefix) throw new Error("You must have a prefix in your nyxx.config.json file!");
-
-        if (config.prefix.startsWith("global")) {
-            let split = config.prefix.split(" ");
-            if (!split[1]) throw new Error("You must provide a prefix!");
-
-            return split[1];
-        } else {
-            return config.prefix;
-        }
-    },
-
-    command: function(guild, command) {
-        if (!config.commands) throw new Error("You must have a commands object in your nyxx.config.json file!");
-
-        if (config.commands[command]) {
-            return config.commands[command];
-        } else {
-            return false;
-        }
-    },
-
-    exists: {
-        guild: function(guild) {
-        }
-    },
-
-    add: {
-        guild: function(guild) {
-            if (!config.database) throw new Error("You must have a database in your nyxx.config.json file!");
-            if (!config.default) throw new Error("You must have a default object in your nyxx.config.json file!");
-
-            // DBL: Database Location
-            global.dbl = config.database.split(" ")[1];
-
-            let contents = fs.readFileSync(global.dbl);
-
-            // Make sure to keep everything else and don't overwrite it.
-            fs.writeFileSync(global.dbl, JSON.stringify({
-                ...JSON.parse(contents),
-                [guild]: config.default
-            }));
-        }
+      global.db = require("./" + split[1]);
     }
+  },
+
+  // Get the prefix of a guild.
+  prefix: function (guild) {
+    if (!config.prefix)
+      throw new Error("You must have a prefix in your nyxx.config.json file!");
+
+    if (config.prefix.startsWith("global")) {
+      let split = config.prefix.split(" ");
+      if (!split[1]) throw new Error("You must provide a prefix!");
+
+      return split[1];
+    } else {
+      return config.prefix;
+    }
+  },
+
+  command: function (guild, command) {
+    if (!config.commands)
+      throw new Error(
+        "You must have a commands object in your nyxx.config.json file!"
+      );
+
+    if (config.commands[command]) {
+      return config.commands[command];
+    } else {
+      return false;
+    }
+  },
+
+  exists: {
+    // Check if a guild exists in the database.
+    guild: function (guild) {},
+  },
+
+  add: {
+    guild: function (guild) {
+      if (!config.database)
+        throw new Error(
+          "You must have a database in your nyxx.config.json file!"
+        );
+      if (!config.default)
+        throw new Error(
+          "You must have a default object in your nyxx.config.json file!"
+        );
+
+      // DBL: Database Location
+      global.dbl = config.database.split(" ")[1];
+
+      let contents = fs.readFileSync(global.dbl);
+
+      // Make sure to keep everything else and don't overwrite it.
+      fs.writeFileSync(
+        global.dbl,
+        JSON.stringify({
+          ...JSON.parse(contents),
+          [guild]: config.default,
+        })
+      );
+    },
+  },
 };
