47358b7ec0b7bc0ba5a5f8535b44868d31e5383d
Error 404 <school-exploits@protonmail.com>
2023-01-20 19:32:50 -0600

    Update

--- cli.js
+++ cli.js
@@ -13,6 +13,15 @@ commander
         message: "What is the name of your project?",
       });
     
+    if (fs.existsSync(name.value)) {
+      const name = await prompts({
+        type: "text",
+        name: "value",
+        message: "That project name already exists! Please choose another name.",
+      });
+    }
+
+    
       const dashboard = await prompts({
         type: "select",
         name: "value",
@@ -32,17 +41,33 @@ commander
           { title: "Local JSON", value: "json" },
         ],
       });
-    
-      if (dashboard.value === "preset") {
-        const preset = await prompts({
-          type: "select",
-          name: "value",
-          message: "Which preset?",
-          choices: [
-            { title: "None [None avalible right now sorry!]", value: "none" },
-          ],
-        });
+
+      if (!fs.existsSync(name.value)) {
+        fs.mkdirSync(name.value);
       }
+
+      fs.writeFileSync(name.value + "/nyxx.config.json", JSON.stringify({
+          "database": "local guilds/db.json",
+          "default": {
+              "welcomeChannel": null,
+              "welcomeMessage": null,
+              "welcomeEnabled": false
+          }
+      }, null, 2));
+
+      // Add dashboard later
+
+      if (fs.existsSync(name.value + "/guilds")) {
+        fs.rm(name.value + "/guilds", { recursive: true });
+
+        fs.mkdirSync(name.value + "/guilds");
+      } else {
+        fs.mkdirSync(name.value + "/guilds");
+      }
+
+      fs.writeFileSync(name.value + "/guilds/db.json", JSON.stringify({}, null, 2));
+
+      console.log("\x1b[32m", "[⚡] Created project " + name.value + " successfully!", "\x1b[0m");
   });
 
 commander.parse(process.argv);
